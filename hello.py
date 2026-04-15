6d2e00226668c0deb8284196429869dbcfd87a18
Big John <win2linux3@gmail.com>
2026-04-14 22:14:45 -0500

    Added network name to code

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
-import platform  # This 'imports' the toolbox for system info
+import platform 
 
-# We are creating 'variables' to store the info the toolbox finds
 machine_type = platform.machine()
 os_name = platform.system()
 version = platform.release()
+network_name = platform.node() # I changed this to lowercase to match below
 
-# This prints a blank line for looks
 print("\n--- SYSTEM REPORT ---")
 
-# We use 'f-strings' (the f before the quotes) to plug variables into text
 print(f"Processor Architecture: {machine_type}")
 print(f"Operating System: {os_name}")
 print(f"OS Version: {version}")
+# Added the variable and fixed the closing quote/parenthesis
+print(f"Network Name: {network_name}") 
 print("---------------------\n")
