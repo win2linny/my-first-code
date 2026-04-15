9507bc8e46f9fdc513fe4e1f733e35c0fb28b14c
Big John <win2linux3@gmail.com>
2026-04-14 22:36:49 -0500

    Added my first if/else logic

--- hello.py
+++ hello.py
@@ -13,3 +13,14 @@ print(f"OS Version: {version}")
 # Added the variable and fixed the closing quote/parenthesis
 print(f"Network Name: {network_name}") 
 print("---------------------\n")
+# Part 2: Interactive Logic
+# input() stops the program and waits for you to type something
+user_name = input("Enter your name: ")
+
+# The 'if' statement checks a condition
+# We use '==' to compare (one '=' is for assigning, two '==' if for checking)
+if user_name == "Big-John":
+    print(f"Welcome back, Master {user_name}. Your iMac is ready.")
+else:
+    print(f"Hello {user_name}, you are logged in as a guest.")    
+    
